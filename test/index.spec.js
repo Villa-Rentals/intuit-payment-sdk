be5973c5e92c3b05a8bac1ca0bd5a82ac0b17142
bolencki13 <bolencki13@gmail.com>
2018-03-27 12:20:06 -0400

    initial commit

---
+++ test/index.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import chai from 'chai'
+
+import Intuit from '../lib/intuitPaymentSDK.js'
+
+chai.expect()
+
+const expect = chai.expect
+
+describe('Given an instance of Library', () => {
+  let lib
+
+  before(() => {
+    lib = new Intuit()
+  })
+})
