654149ad51c08aabb567af64a291e7a68659e779
bolencki13 <bolencki13@gmail.com>
2018-03-29 12:45:00 -0400

    create bank from a token (untested)

--- src/controllers/bankAccount.js
+++ src/controllers/bankAccount.js
@@ -8,7 +8,6 @@ export default class BankAccount extends Base {
    *
    * Create a bank account object through the Intuit Quickbooks payment API
    *
-   *
    * Required Options:
    *   - `bank`
    *     - `name`           name of person
@@ -16,7 +15,8 @@ export default class BankAccount extends Base {
    *     - `accountNumber`  account number for account
    *     - `accountType`    account type (i.e. `PERSONAL_CHECKING` or `PERSONAL_SAVINGS`)
    *     - `phone`          phone number of person
-   *   - `customerID`     identifer for the customer
+   *   - `customerID`       identifier for the customer
+   *   - `requestID`        identifier for the network request tracking
    *
    * Examples:
    *
@@ -26,14 +26,47 @@ export default class BankAccount extends Base {
    *        "accountNumber": "XXXX4534",
    *        "accountType": "PERSONAL_CHECKING",
    *        "phone": "6047296480"
-   *     });
+   *     }, 'customer-id', 'request-id');
    *
    * @method
    * @param {object} bank
+   * @param {string} customerID
+   * @param {string} requestID
    * @access public
    * @return {Promise} promise containing network response object
    */
   create (bank, customerID, requestID) {
     return this.request('POST', `/customers/${customerID}/bank-accounts`, bank, requestID)
+  }
+
+  /**
+   * `BankAccount` `createFromToken` method.
+   *
+   * Create a bank account object from a token through the Intuit Quickbooks payment API
+   *
+   * Required Options:
+   *   - `token`       tokenized version of the bank account
+   *   - `customerID`  identifier for the customer
+   *   - `requestID`   identifier for the network request tracking
+   *
+   * Examples:
+   *
+   *     BankAccount().createFromToken(
+   *        'some-token',
+   *        'customer-id',
+   *        'request-id'
+   *     });
+   *
+   * @method
+   * @param {string} token
+   * @param {string} customerID
+   * @param {string} requestID
+   * @access public
+   * @return {Promise} promise containing network response object
+   */
+  createFromToken (token, customerID, requestID) {
+    return this.request('POST', `/customers/${customerID}/bank-accounts/createFromToken`, {
+      value: token
+    }, requestID)
   }
 }
