1a3afc939e56453d6f1a3fe659e55b7df4f48e7c
bolencki13 <bolencki13@gmail.com>
2018-03-29 16:56:26 -0400

    credit card tested

---
+++ test/controllers/creditCard.test.js
@@ -0,0 +1,70 @@
+'use strict'
+
+import { assert } from 'chai'
+import Intuit from '../../lib/intuitPaymentSDK.js'
+
+describe('Given an instance of CreditCard', () => {
+  let lib
+  let cardID
+
+  before(() => {
+    lib = new Intuit({
+      production: false,
+      version: 'v4'
+    }).CreditCard(process.env.ACCESS_TOKEN, process.env.REALM_ID)
+  })
+
+  it('add a credit card', () => {
+    return lib.create({
+      'number': '6011111111111117',
+      'expMonth': '12',
+      'expYear': '2026',
+      'name': 'Test User',
+      'address': {
+        'streetAddress': '1245 Hana Rd',
+        'city': 'Richmond',
+        'region': 'VA',
+        'country': 'US',
+        'postalCode': '44112'
+      }
+    }, 'customerID')
+      .then(({data}) => {
+        cardID = data.id
+        assert.equal(data.name, 'Test User')
+      })
+      .catch((error) => {
+        assert.isNull(error, 'Error should be null')
+      })
+  })
+
+  it('get a credit card', () => {
+    return lib.get(cardID, 'customerID')
+      .then(({data}) => {
+        assert.equal(data.id, cardID)
+      })
+      .catch((error) => {
+        assert.isNull(error, 'Error should be null')
+      })
+  })
+
+  it('get all credit cards', () => {
+    return lib.all('customerID')
+      .then(({data}) => {
+        assert.isArray(data, 'Response is array')
+        assert.equal(data[0].id, cardID)
+      })
+      .catch((error) => {
+        assert.isNull(error, 'Error should be null')
+      })
+  })
+
+  it('delete a credit card', () => {
+    return lib.remove(cardID, 'customerID')
+      .then(() => {
+        assert.isTrue(true)
+      })
+      .catch((error) => {
+        assert.isNull(error, 'Error should be null')
+      })
+  })
+})
